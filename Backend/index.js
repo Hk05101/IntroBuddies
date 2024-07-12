6af7f087dc6c74a23a1a8f9780b6ab096d144091
VedantAnand17 <vedantanand0077@gmail.com>
2024-07-12 20:06:40 +0530

    please chalja

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,7 @@ import cors from 'cors';
 import resourceRoute from './route/resource.route.js';
 import userRoute from './route/user.route.js'
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
@@ -29,12 +30,15 @@ app.use('/resource', resourceRoute)
 app.use('/user', userRoute)
 
 //deployment code
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+console.log(__dirname);
 
 if(process.env.NODE_ENV === "production") {
     const dirPath = path.resolve();
-    app.use(express.static("./Frontend/dist"));
+    app.use(express.static(path.join(__dirname, '/Frontend/dist')));
     app.get("*",(req,res) => {
-        res.sendFile(path.resolve(dirPath,"./Frontend/dist","index.html"));
+        res.sendFile(path.join(__dirname, '/Frontend/dist/index.html'));
     })
 }
 
